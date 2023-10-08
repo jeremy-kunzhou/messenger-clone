4c76fc5876183b6d319f0defec959a6f4a65a8ca
jeremy <clirochina@gmail.com>
2023-10-08 23:15:30 +1100

    finish

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import ToasterContext from "./context/ToasterContext";
+import AuthContext from "./context/AuthContext";
+import ActiveStatus from "./components/ActiveStatus";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Messenger Clone",
+  description: "Messenger Clone",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/* <body className={inter.className}> */}
+      <body>
+        <AuthContext>
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  );
+}
